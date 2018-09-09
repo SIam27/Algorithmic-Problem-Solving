6ce0ccfa3005a5b89276fbf437bff53e9ab49926
siam27 <sanzidsiam@gmail.com>
2018-09-08 23:40:19 -0400

    testing

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+cout <<" please enter your name and age separtaed by a space";
+string name;
+int age;
+cin >> name;
+cin >> age;
+cout << "hello, " << name << " , you are " << age << "years old";
+return 0;
+}
+

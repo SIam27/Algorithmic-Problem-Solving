70406ca77ab0ff36a733196396fcbe6aeeeb4b5c
siam27 <sanzidsiam@gmail.com>
2018-09-08 23:35:14 -0400

    new file

---
+++ pori.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+cout <<" how many inch tall are you?, " ;
+int height;
+cin >> height;
+cout <<"you are" << height/12 << " feet, " << height%12 << " inch tall";
+
+}

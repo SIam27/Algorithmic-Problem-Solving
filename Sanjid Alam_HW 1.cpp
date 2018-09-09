8de3c44e79a06080838b3f63bf970ef854cc27a8
SIam27 <43103700+SIam27@users.noreply.github.com>
2018-09-08 23:20:02 -0400

    Add files via upload

---
+++ Sanjid Alam_HW 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main () {
+cout << "Enter the base of the right triangle ";
+double base;
+cin >> base;
+cout << "Enter the height of the right triangle ";
+double height;
+cin >> height;
+cout << "the area is " << 0.5 * base * height << endl;
+cout << "How many inches tall are you? ";
+int x;
+cin >> x;
+cout << "that is " << x/12 << " feet " << x%12 << " inch tall";
+}
